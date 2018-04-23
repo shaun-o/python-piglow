0c1a8c5a276bbf48aa86b36ee7891fc1478bdd91
Shaun Oates <shaunoates@Shauns-MacBook.local>
2018-04-23 23:31:04 +0100

    Changed slightly

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
+import sys
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Shaun This is Python Hello World!' + sys.version
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
