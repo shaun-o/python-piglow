d1b8a0601ee65e6479519f0fd19bf802a63e6ce1
Shaun Oates <shaunoates@Shauns-MacBook.local>
2018-04-24 22:46:11 +0100

    build force

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Shaun This is Python Hello World!' + sys.version
+    return 'Shaun This is  Python Hello World!' + sys.version
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
