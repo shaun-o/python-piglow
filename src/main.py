7681d6d636cb89717ffdb763ce97672616e0a210
Shaun Oates <shaunoates@Shauns-MacBook.local>
2018-04-25 22:56:17 +0100

    all on and an off method

--- src/main.py
+++ src/main.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    piglow.red(64)
+    piglow.all(64)
     piglow.show()
     return 'Shaun This is  Python Hello World!' + sys.version
 
 @app.route('/off')
 def all_off():
-    piglow.all(0)
+    piglow.off()
     piglow.show()
+    return 'All off now'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
